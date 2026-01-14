c8974e5df27fa3e38ec7886bd521041f05b5d027
Sparsh-23-aiml <sparsh.sharma_cs.aiml23@gla.ac.in>
2026-01-14 11:17:53 +0530

    Allow images from R2 domain in Next.js config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'pub-cf3d1dba4a1f4076a4119c604b1587ab.r2.dev',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.r2.dev',
+        pathname: '/**',
+      }
+    ],
+  },
   typescript: {
     // !! WARN !! - Dangerously allow production builds to successfully complete even if
     // your project has type errors.
